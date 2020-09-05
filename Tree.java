9f40e55b094fd4b9dea135e19bb3323c9d12fed1
Mireia Farre <67146405+farremireia@users.noreply.github.com>
2020-09-05 15:51:13 +0100

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //introduce self and print tree
+    System.out.println("Hey, everyone! I'm Mireia!");
+    System.out.println("Let's plant a tree!");
+    System.out.println("    ####   ");
+    System.out.println("  ###/#### ");
+    System.out.println(" ###|/#### ");
+    System.out.println("   #||/## ");
+    System.out.println("    ||# ");
+    System.out.println("    ||___ ");
+  }
+}
